2bc82d3e65149ad2dbd016308c9a3da1d3660100
Rodrigo Barros <rodrigo.gabriel@l5.com.br>
2024-07-14 15:15:55 -0300

    Melhoria da feature de autocomplete.
    - corrigido o problema de ficar mostrando aviso de arquivo não encontrado.

--- completion.sh
+++ completion.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
-. constants.sh
-_file_watcher()
-{
-    local prefix="$3"
-    local filter="$2"
-    local backup_files=""
-    local compfilter=""
-    local file_dir=""
+if which file_watcher > /dev/null ;then
+    . $(dirname $(readlink -f $(which file_watcher)))/constants.sh
+    _file_watcher()
+    {
+        local prefix="$3"
+        local filter="$2"
+        local backup_files=""
+        local compfilter=""
+        local file_dir=""
 
-    [ "$filter" != "" ] && compfilter="-- $filter"
+        [ "$filter" != "" ] && compfilter="-- $filter"
 
-    for file in $(find $BACKUP_FOLDER -type f);do
-        backup_file=$(echo $(basename $file) | sed 's|\\|/|' | sed 's|\\|/|g')
-        backup_files+="$backup_file "
-    done
+        if [ -d $BACKUP_FOLDER ];then
+            for file in $(find $BACKUP_FOLDER -type f);do
+                backup_file=$(echo $(basename $file) | sed 's|\\|/|' | sed 's|\\|/|g')
+                backup_files+="$backup_file "
+            done
+        fi
 
 
-    COMPREPLY=()
+        COMPREPLY=()
 
-    case $prefix in
-        --restore*)
-            COMPREPLY+=($prefix)
-            COMPREPLY=($(compgen -W "$backup_files" $compfilter));;
-    esac
+        case $prefix in
+            --restore*)
+                COMPREPLY+=($prefix)
+                COMPREPLY=($(compgen -W "$backup_files" $compfilter));;
+        esac
 
-    case $filter in
-        --*)
-            COMPREPLY=($(compgen -W "--help --service --restore" $compfilter));;
-    esac
-}
+        case $filter in
+            --*)
+                COMPREPLY=($(compgen -W "--help --service --restore" $compfilter));;
+        esac
+    }
 
-complete -F _file_watcher file_watcher
+    complete -F _file_watcher file_watcher
+fi
